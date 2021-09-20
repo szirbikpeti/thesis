4ecb21a81638af3928575efc0ad7bfcfde1df136
Szirbik Péter <h880681@stud.u-szeged.hu>
2021-09-20 14:36:54 +0200

    Improve feedback page

--- WorkoutApp/Angular/src/app/pages/feedbacks/feedbacks.component.ts
+++ WorkoutApp/Angular/src/app/pages/feedbacks/feedbacks.component.ts
@@ -1,6 +1,9 @@
 import { Component, } from '@angular/core';
 import {AdminService} from "../../services/admin.service";
 import {FeedbackModel} from "../../models/FeedbackModel";
+import {getPicture} from "../../utility";
+import {StateService} from "../../services/state.service";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-feedbacks',
@@ -11,7 +14,10 @@ export class FeedbacksComponent  {
 
   feedbacks: FeedbackModel[];
 
-  constructor(private _admin: AdminService) {
+  getPicture = getPicture;
+
+  constructor(private _admin: AdminService, public _state: StateService,
+              public sanitizer: DomSanitizer) {
     _admin.listFeedbacks()
       .subscribe(feedbacks => this.feedbacks = feedbacks);
   }
